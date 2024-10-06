ca53197ffdecaf45426ef007fe212f390f118b01
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-10-06 18:05:01 +0300

    Update من بالمكالمة.py

--- YukkiMusic/plugins/Abod/من بالمكالمة.py
+++ YukkiMusic/plugins/Abod/من بالمكالمة.py
@@ -4,13 +4,13 @@ import asyncio
 from pyrogram.types import VideoChatEnded, Message
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from YukkiMusic.core.call import Mody
+from YukkiMusic.core.call import Yukki
 from YukkiMusic.utils.database import *
 from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError,AlreadyJoinedError)
 
 @app.on_message(filters.regex("^(مين في الكول|من في الكول|من بالمكالمه|من بالمكالمة|من في المكالمه|من في المكالمة|الصاعدين)$"))
 async def strcall(client, message):
-    assistant = await group_assistant(Mody,message.chat.id)
+    assistant = await group_assistant(Yukki,message.chat.id)
     try:
         await assistant.join_group_call(message.chat.id, AudioPiped("./ZeMusic/assets/call.mp3"), stream_type=StreamType().pulse_stream)
         text="<b>⟡ الموجودين في المكالمه :</b>\n\n"
