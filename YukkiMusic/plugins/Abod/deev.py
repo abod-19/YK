b80f18871bfb34f0ba8ea8589204ac64acfed8a0
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-10-06 17:54:28 +0300

    Update deev.py

--- YukkiMusic/plugins/Abod/deev.py
+++ YukkiMusic/plugins/Abod/deev.py
@@ -5,7 +5,7 @@ import requests
 from config import START_IMG_URL, OWNER_ID
 from pyrogram import Client, filters, emoji
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from ZeMusic import app
+from YukkiMusic import app
 
 @app.on_message(filters.text & filters.regex(r"^\.$"))
 async def huhh(client: Client, message: Message):
