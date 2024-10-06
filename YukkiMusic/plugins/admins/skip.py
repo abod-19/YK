a0e14a39a5f2766cf52492c0a3aba9744992acdc
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-10-06 15:46:21 +0300

    Update skip.py

--- YukkiMusic/plugins/admins/skip.py
+++ YukkiMusic/plugins/admins/skip.py
@@ -27,8 +27,8 @@ from YukkiMusic.utils.thumbnails import gen_thumb
 # Commands
 SKIP_COMMAND = get_command("SKIP_COMMAND")
 
-
-@app.on_message(filters.command(SKIP_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["skip", "cskip", "next", "cnext"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["تخطي","التالي"],"") & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
     if not len(message.command) < 2:
