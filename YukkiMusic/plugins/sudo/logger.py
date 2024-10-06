0a0d50fa2696ceeaf14615c96368242b3bd4a8b4
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-10-06 16:34:33 +0300

    Update logger.py

--- YukkiMusic/plugins/sudo/logger.py
+++ YukkiMusic/plugins/sudo/logger.py
@@ -21,7 +21,7 @@ from YukkiMusic.utils.decorators.language import language
 LOGGER_COMMAND = get_command("LOGGER_COMMAND")
 
 
-@app.on_message(filters.command(LOGGER_COMMAND) & SUDOERS)
+@app.on_message(filters.command(("السجل")) & SUDOERS)
 @language
 async def logger(client, message, _):
     usage = _["log_1"]
@@ -29,10 +29,10 @@ async def logger(client, message, _):
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip()
     state = state.lower()
-    if state == "enable":
+    if state == "تفعيل":
         await add_on(config.LOG)
         await message.reply_text(_["log_2"])
-    elif state == "disable":
+    elif state == "تعطيل":
         await add_off(config.LOG)
         await message.reply_text(_["log_3"])
     else:
