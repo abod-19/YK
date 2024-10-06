fc5b517cce588c69c38527beb063c401007b9345
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-10-06 14:35:32 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -95,7 +95,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/abod-19/YK.git",
+    "https://github.com/abod-19/YK",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
