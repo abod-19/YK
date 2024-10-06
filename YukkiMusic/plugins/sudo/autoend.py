cc95edb9e887863536aa9b1134b76d33dd3c8ae3
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-10-06 16:21:40 +0300

    Update autoend.py

--- YukkiMusic/plugins/sudo/autoend.py
+++ YukkiMusic/plugins/sudo/autoend.py
@@ -18,19 +18,19 @@ from YukkiMusic.utils.database import autoend_off, autoend_on
 AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
 
 
-@app.on_message(filters.command(AUTOEND_COMMAND) & SUDOERS)
+@app.on_message(filters.command((مغادرة)) & SUDOERS)
 async def auto_end_stream(client, message):
-    usage = "**ᴜsᴀɢᴇ:**\n\n/autoend [enable|disable]"
+    usage = "**ᴜsᴀɢᴇ:**\n\n/مغادرة [تفعيل|تعطيل]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip()
     state = state.lower()
-    if state == "enable":
+    if state == "تفعيل":
         await autoend_on()
         await message.reply_text(
             "Aᴜᴛᴏ Eɴᴅ Sᴛʀᴇᴀᴍ Eɴᴀʙʟᴇᴅ.\n\nBᴏᴛ ᴡɪʟʟ ʟᴇᴀᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴀғᴛᴇʀ 30 sᴇᴄᴏɴᴅs ɪғ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ ᴡɪᴛʜ ᴀ ᴡᴀʀɴɪɴɢ ᴍᴇssᴀɢᴇ.."
         )
-    elif state == "disable":
+    elif state == "تعطيل":
         await autoend_off()
         await message.reply_text("ᴀᴜᴛᴏᴇɴᴅ ᴅɪsᴀʙʟᴇᴅ")
     else:
