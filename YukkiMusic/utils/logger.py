774f799ef4e633ab41379bd41014991abb6ce5fc
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-10-06 18:51:14 +0300

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -14,24 +14,29 @@ from YukkiMusic.utils.database import is_on_off
 
 async def play_logs(message, streamtype):
     if await is_on_off(LOG):
-        if message.chat.username:
-            chatusername = f"@{message.chat.username}"
+        if message.from_user:
+            ne = "تم التشغيل في المجموعة"
+            user_id = message.from_user.id
+            user_mention = message.from_user.mention
+            user_username = message.from_user.username
         else:
-            chatusername = "ᴘʀɪᴠᴀᴛᴇ ɢʀᴏᴜᴘ"
-
+            ne = "تم التشغيل في القناة"
+            user_id = "مجهول"
+            user_mention = "مجهول"
+            user_username = "مجهول"
         logger_text = f"""
-**{app.mention} ᴘʟᴀʏ ʟᴏɢ**
+<b>{ne}</b>
 
-**ᴄʜᴀᴛ ɪᴅ :** `{message.chat.id}`
-**ᴄʜᴀᴛ ɴᴀᴍᴇ :** {message.chat.title}
-**ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :** {chatusername}
+<b>شات ايدي :</b> <code>{message.chat.id}</code>
+<b>الاسم :</b> {message.chat.title}
+<b>اليوزر :</b> @{message.chat.username}
 
-**ᴜsᴇʀ ɪᴅ :** `{message.from_user.id}`
-**ɴᴀᴍᴇ :** {message.from_user.mention}
-**ᴜsᴇʀɴᴀᴍᴇ :** @{message.from_user.username}
+<b>ايدي :</b> <code>{user_id}</code>
+<b>الاسم :</b> {user_mention}
+<b>يوزر :</b> @{user_username}
 
-**ǫᴜᴇʀʏ :** {message.text.split(None, 1)[1]}
-**sᴛʀᴇᴀᴍᴛʏᴘᴇ :** {streamtype}"""
+<b>الاغنيه :</b> {message.text.split(None, 1)[1]}
+<b>اسم المشغل :</b> {streamtype}"""
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
