edfb6d88928677d2bc02c1d42827ec11769db096
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-10-11 18:17:53 +0300

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -29,7 +29,7 @@ def start_pannel(_):
 
 def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
-        [InlineKeyboardButton(text="الأوامر", callback_data="prvett")]
+        [InlineKeyboardButton(text="الأوامر", callback_data="azzzback")]
     ]
     #buttons.append(
         #[
